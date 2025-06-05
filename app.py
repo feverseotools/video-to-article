597059a0573f1188b99593490f415d9c7811df0d
Jakub Motyka <jakub.motyka@feverup.com>
2025-06-05 16:04:54 +0200

    Previous one with modified date

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
 
 if not st.session_state.authenticated:
-    pw = st.text_input("Introduce la contraseña para acceder (v05/06/2025 15:58h)", type="password")
+    pw = st.text_input("Introduce la contraseña para acceder (v05/06/2025 16:04Sh)", type="password")
     if pw == PASSWORD:
         st.session_state.authenticated = True
         st.rerun()
