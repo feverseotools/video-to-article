9cd0fd2911bdb611f7fc443fbbbdbff0bacd22b7
Jakub Motyka <jakub.motyka@feverup.com>
2025-06-09 16:35:18 +0200

    Updating hour

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
 
 if not st.session_state.authenticated:
-    pw = st.text_input("Enter your super-ultra secret password (v09/06/2025 10:15h)", type="password")
+    pw = st.text_input("Enter your super-ultra secret password (v09/06/2025 16:35h)", type="password")
     if pw == PASSWORD:
         st.session_state.authenticated = True
         st.rerun()
