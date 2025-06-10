9a44a7174ecdb3ce20932e4cae9b295649b7c975
Jakub Motyka <jakub.motyka@feverup.com>
2025-06-10 13:16:47 +0200

    Fixing folder of category prompts

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
 
 if not st.session_state.authenticated:
-    pw = st.text_input("Enter your super-ultra secret password (v10/06/2025 13:14h)", type="password")
+    pw = st.text_input("Enter your super-ultra secret password (v10/06/2025 13:16h)", type="password")
     if pw == PASSWORD:
         st.session_state.authenticated = True
         st.rerun()
