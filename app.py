dbd5f2ea48dc1b89b7bd812ca6f5c4c1656fc837
Jakub Motyka <jakub.motyka@feverup.com>
2025-07-02 11:13:20 +0200

    fix/same-previous-one-only-modified-hour-of-last-version

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
 if not st.session_state.authenticated:
     pw = st.text_input(
-        "Enter your super-ultra secret password (v02/07/2025 11:10h)",
+        "Enter your super-ultra secret password (v02/07/2025 11:12h)",
         type="password"
     )
     if pw == PASSWORD:
